b2d67b3503ad1d05dc436d0245b61f2f007f111b
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-05-21 10:26:11 +0545

    Widgets Added

---
+++ lib/widget/main_wrapper.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconly/iconly.dart';
+
+import '../bloc/bottom_nav_cubit.dart';
+import '../pages/pages.dart';
+
+class MainWrapper extends StatefulWidget {
+  const MainWrapper({super.key});
+
+  @override
+  State<MainWrapper> createState() => _MainWrapperState();
+}
+
+class _MainWrapperState extends State<MainWrapper> {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  /// Top Level Pages
+  final List<Widget> topLevelPages = const [
+    HomePage(),
+    FavoritePage(),
+    NotificationsPage(),
+    ProfilePage(),
+  ];
+
+  /// on Page Changed
+  void onPageChanged(int page) {
+    BlocProvider.of<BottomNavCubit>(context).changeSelectedIndex(page);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 2, 2, 2),
+      appBar: _mainWrapperAppBar(),
+      body: _mainWrapperBody(),
+      bottomNavigationBar: _mainWrapperBottomNavBar(context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+      floatingActionButton: _mainWrapperFab(),
+    );
+  }
+
+  // Bottom Navigation Bar - MainWrapper Widget
+  BottomAppBar _mainWrapperBottomNavBar(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.black,
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _bottomAppBarItem(
+                    context,
+                    defaultIcon: IconlyLight.home,
+                    page: 0,
+                    label: "Home",
+                    filledIcon: IconlyBold.home,
+                  ),
+                  _bottomAppBarItem(
+                    context,
+                    defaultIcon: IconlyLight.heart,
+                    page: 1,
+                    label: "Favorite",
+                    filledIcon: IconlyBold.heart,
+                  ),
+                  _bottomAppBarItem(
+                    context,
+                    defaultIcon: IconlyLight.notification,
+                    page: 2,
+                    label: "Notification",
+                    filledIcon: IconlyBold.notification,
+                  ),
+                  _bottomAppBarItem(
+                    context,
+                    defaultIcon: IconlyLight.profile,
+                    page: 3,
+                    label: "Profile",
+                    filledIcon: IconlyBold.profile,
+                  ),
+                ],
+              ),
+            ),
+            // const SizedBox(
+            //   width: 80,
+            //   height: 20,
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Floating Action Button - MainWrapper Widget
+  FloatingActionButton _mainWrapperFab() {
+    return FloatingActionButton(
+      onPressed: () {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            behavior: SnackBarBehavior.floating,
+            backgroundColor: Color.fromARGB(255, 7, 7, 7),
+            content: Text("New post will generate in upcoming 2 mins 📮"),
+          ),
+        );
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+      backgroundColor: Colors.amber,
+      child: const Icon(Icons.add),
+    );
+  }
+
+  // App Bar - MainWrapper Widget
+  AppBar _mainWrapperAppBar() {
+    return AppBar(
+      backgroundColor: Colors.black,
+      title: const Text("BottomNavigationBar with Cubit"),
+    );
+  }
+
+  // Body - MainWrapper Widget
+  PageView _mainWrapperBody() {
+    return PageView(
+      onPageChanged: (int page) => onPageChanged(page),
+      controller: pageController,
+      children: topLevelPages,
+    );
+  }
+
+  // Bottom Navigation Bar Single item - MainWrapper Widget
+  Widget _bottomAppBarItem(
+    BuildContext context, {
+    required defaultIcon,
+    required page,
+    required label,
+    required filledIcon,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        BlocProvider.of<BottomNavCubit>(context).changeSelectedIndex(page);
+
+        pageController.animateToPage(page,
+            duration: const Duration(milliseconds: 10),
+            curve: Curves.fastLinearToSlowEaseIn);
+      },
+      child: SizedBox(
+        height: 56,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              context.watch<BottomNavCubit>().state == page
+                  ? filledIcon
+                  : defaultIcon,
+              color: context.watch<BottomNavCubit>().state == page
+                  ? Colors.amber
+                  : Colors.grey,
+              size: 26,
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            Text(
+              label,
+              style: GoogleFonts.aBeeZee(
+                color: context.watch<BottomNavCubit>().state == page
+                    ? Colors.amber
+                    : Colors.grey,
+                fontSize: 13,
+                fontWeight: context.watch<BottomNavCubit>().state == page
+                    ? FontWeight.w600
+                    : FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
